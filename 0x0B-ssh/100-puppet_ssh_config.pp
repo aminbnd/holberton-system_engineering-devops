0dbd22c2cc15a8c486428f2755ea5878b5ad178f
aminbnd <2212@holbertonschool.com>
2021-04-05 14:29:06 +0200

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,3 +5,9 @@ path     => '/etc/ssh/ssh_config',
 line     => '    PasswordAuthentication no',
 replace  => true
 }
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true
+}
