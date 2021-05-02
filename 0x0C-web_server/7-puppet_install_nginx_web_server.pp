ba2beb64f07ee793a1bb045543fedc24d9d00794
aminbnd <2212@holbertonschool.com>
2021-05-02 16:00:30 +0200

    Task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,29 @@
-# Scrpit that setup Nginx web server on web-01 server 
+# Script that setup a nginx web server on our server + redirection.
 
 package { 'nginx':
-    ensure   => present,
-    provider => 'apt'
+  ensure   => present,
+  provider => 'apt'
 }
 
-# Index page content
-file { '/var/www/html/index.nginx-debian.html'
-    ensure  => present,
-    path    => '/var/www/html/index.nginx-debian.html'
-    content => 'Holberton School'
+# Index page
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School'
 }
 
-# Redirection
-file_line { 'Nyan Cat': 
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default server;',
-    line    => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+# Redirect to fabulous Rick Astley page
+file_line { 'Nyan Cat':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
-service {'nginx':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    require    => Package['nginx'],
-    subscribe  => File_line['Nyan Cat']
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Nyan Cat']
 }
