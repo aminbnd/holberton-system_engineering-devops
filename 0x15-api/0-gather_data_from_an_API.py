023a1ed978103db2e7a27f19462d8cf8111a2c93
aminbnd <2212@holbertonschool.com>
2021-05-03 19:58:24 +0200

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""
-Module that requests, for a given
-employee id, a number of information
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = sys.argv[1]
-    employee_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-    employee = requests.get(employee_url).json()
-    todos = requests.get(todos_url).json()
-
-    completed = [task.get("title") for task in todos if
-                 task.get("completed") is True]
-    print("Employee {} is done with tasks ({}/{}):".format(
-        employee[0].get("name"), len(completed), len(todos)))
-    [print("\t {}".format(comp)) for comp in completed]
-
+    completed = [task.get("title") for task in todos if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(compl)) for compl in completed]
