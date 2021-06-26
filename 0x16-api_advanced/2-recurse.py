a1527beb2bf23bd12d4357bd6860435666643e81
Amin Bondi <70951069+aminbnd@users.noreply.github.com>
2021-06-26 10:25:49 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 containing the titles of all hot articles for a given subreddit. """
 
 import requests
-headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/Brandixitor)"}
+headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/Tristan_001)"}
 
 
 def recurse(subreddit, hot_list=[], after=None):
